a2ad11591d799f673239a52b857b34c73a170dbc
Prosper Osagie-Amayo <prosperosagieamayo@gmail.com>
2018-07-19 11:24:57 -0700

    i don't need this, get out of here

--- src/screens/home.screen.tsx
+++ src/screens/home.screen.tsx
@@ -1,7 +1,6 @@
 import * as React from 'react';
 import { Text, View } from 'react-native';
 import { connect } from 'react-redux';
-import { Dispatch } from 'redux';
 
 class HomeScreen extends React.Component<{}, {}> {
   public render(): JSX.Element {
