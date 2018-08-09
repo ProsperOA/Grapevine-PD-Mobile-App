a17066b91b1f29ac45a1d2e0d647c96e22a2f1ae
Prosper Osagie-Amayo <prosperosagieamayo@gmail.com>
2018-08-08 18:20:12 -0700

    add home layout, custom header with camera

--- src/screens/home.screen.tsx
+++ src/screens/home.screen.tsx
@@ -1,12 +1,61 @@
 import * as React from 'react';
-import { Text, View } from 'react-native';
+import { Text, TouchableOpacity, View } from 'react-native';
+import { Icon, Header } from 'react-native-elements';
+import { Permissions, PictureResponse } from 'expo';
 import { connect } from 'react-redux';
 
-class HomeScreen extends React.Component<{}, {}> {
+import Camera from '../containers/camera.container';
+import { PRIMARY } from '../shared/styles';
+
+interface HomeState {
+  hasCameraPermission: boolean;
+}
+
+class HomeScreen extends React.Component<{}, HomeState> {
+  public state: Readonly<HomeState> = {
+    hasCameraPermission: false
+  };
+
+  public onHeaderCameraButtonPress = async (): Promise<any> => {
+    const { status } = await Permissions.askAsync(Permissions.CAMERA);
+    this.setState({ hasCameraPermission: status === 'granted' });
+  };
+
+  public onTakePicture = ({uri, base64}: PictureResponse): void => {
+    this.setState({ hasCameraPermission: false });
+  };
+
+  public renderHeaderCameraButton = (): JSX.Element => (
+    <TouchableOpacity onPress={() => this.onHeaderCameraButtonPress()}>
+      <Icon name="camera-alt" color="#fff" />
+    </TouchableOpacity>
+  );
+
   public render(): JSX.Element {
+    if (this.state.hasCameraPermission) {
+      return (
+        <Camera
+          onTakePicture={this.onTakePicture}
+          close={() => this.setState({ hasCameraPermission: false })} />
+      );
+    }
+
     return (
-      <View>
-        <Text>Home</Text>
+      <View style={{ flex: 1 }}>
+        <Header
+          outerContainerStyles={{ backgroundColor: PRIMARY }}
+          leftComponent={this.renderHeaderCameraButton()}
+          centerComponent={{ text: 'Grapevine PD', style: { color: '#fff', fontSize: 20, fontWeight: '600' } }} />
+        <View style={{ flex: 1, padding: 15 }}>
+          <View style={{ flex: 0.1 }}>
+            <Text style={{ fontSize: 25, fontWeight: '600' }}>
+              Your Images
+            </Text>
+          </View>
+          <View style={{ flex: 0.9, padding: 15 }}>
+            <Text>images...</Text>
+          </View>
+        </View>
       </View>
     );
   }
